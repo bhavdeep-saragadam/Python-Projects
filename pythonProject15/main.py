48c167a219283ad1ab9a158bbc3036a6f6cb95f1
Bhavdeep <58917780+bhavdeep-saragadam@users.noreply.github.com>
2021-06-20 14:35:32 -0400

    Made a covid-19 tracker 🦠🦠🦠

--- pythonProject15/main.py
+++ pythonProject15/main.py
@@ -5,7 +5,9 @@ covid19 = COVID19Py.COVID19()
 covid19 = COVID19Py.COVID19(data_source="jhu")
 
 
-data = covid19.getLatest()
+
+
+# This is where you could specify what country you would want.
 
 cont = covid19.getLocationByCountryCode('IN', timelines=True)
 
